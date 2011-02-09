9f92af22730e17e53a2cd5b38e2f8fddcef1a666
Malcolm Locke <malc@wholemeal.co.nz>
2011-02-09 22:25:55 +1300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fulcrum::Application.config.secret_token = '2f0d0a139bff42c286f5f4af2910d03aebd461f7d72bab018d6d9095f7f578c04b2f8041923eeb62e2246efbaf7468c3e93f65f3fa007f06b95ff867a52330bd'
