b6552325b200936cb3f627cf31add83a51cec05f
Malcolm Locke <malc@wholemeal.co.nz>
2011-05-05 11:40:10 +1200

    Add heroku as production mail domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,7 @@ Fulcrum::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'fulcrum.heroku.com' }
 
   # Enable threaded mode
   # config.threadsafe!
