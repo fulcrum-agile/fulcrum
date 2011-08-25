bbc117f7052a5abd622e8181db36fab2478f9f03
Rico Sta. Cruz <rico@ambiescent.com>
2011-08-26 01:03:18 +0800

    Add provisions for using SendGrid on Heroku, and add instructions to the README on Heroku deployment

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "please-change-me@config-initializers-devise.com"
+  config.mailer_sender = ENV['MAILER_SENDER'] || "please-change-me@config-initializers-devise.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
---
+++ config/initializers/sendgrid.rb
@@ -0,0 +1,11 @@
+# Configure Heroku to use SendGrid.
+if ENV['SENDGRID_USERNAME']
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
+end
