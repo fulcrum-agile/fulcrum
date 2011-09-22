2ff810d49c46e7513444c2a1812645d857811b0c
Edwin Cruz <edwin.cruz@crowdint.com>
2011-09-21 20:25:09 -0700

    Fixing bug when resending confirmation instructions

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -3,12 +3,18 @@ class ConfirmationsController < Devise::ConfirmationsController
   def show
     self.resource = resource_class.confirm_by_token(params[:confirmation_token])
     
-    if resource.errors.empty?
+    if resource.valid? && resource.errors.empty?
       set_flash_message :notice, :confirmed
       redirect_to edit_user_password_path(:reset_password_token => resource.reset_password_token)
     else
-      render_with_scope :new
+      set_flash_message :notice, :invalid_token
+      render 'devise/confirmations/new'
     end
+  end
+
+  def new
+    self.resource = User.new
+    render 'devise/confirmations/new'
   end
 
 end
