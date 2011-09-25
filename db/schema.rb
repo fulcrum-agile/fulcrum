4a4f4a6b119392431b317f814ea67f59aaf8f273
Malcolm Locke <malc@wholemeal.co.nz>
2011-09-25 23:31:11 +1300

    Bring schema version up to date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110914192131) do
+ActiveRecord::Schema.define(:version => 20110914211417) do
 
   create_table "changesets", :force => true do |t|
     t.integer  "story_id"
