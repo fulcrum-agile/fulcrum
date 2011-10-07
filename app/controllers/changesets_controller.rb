4f0a8feb8a6c4dad068f4150570ce1f54088d1da
wasabhi <abhinav.keswani@trineo.co.nz>
2011-10-07 21:44:37 +1300

    Permits anon access to public project changesets

--- app/controllers/changesets_controller.rb
+++ app/controllers/changesets_controller.rb
@@ -1,6 +1,8 @@
 class ChangesetsController < ApplicationController
+  skip_before_filter :authenticate_user!, :only => [:index]
+  before_filter :permit_public_project, :only => [:index]
+
   def index
-    @project = current_user.projects.find(params[:project_id])
     scope = @project.changesets.scoped
     scope = scope.where('id <= ?', params[:to]) if params[:to]
     scope = scope.where('id > ?', params[:from]) if params[:from]
--- test/functional/changesets_controller_test.rb
+++ test/functional/changesets_controller_test.rb
@@ -24,4 +24,21 @@ class ChangesetsControllerTest < ActionController::TestCase
     assert_response :success
     assert_equal [@changeset], assigns(:changesets)
   end
+
+  test "should get all for anonymous users in json format for public projects" do
+    @project.public = true
+    @project.save
+    get :index, :project_id => @project.to_param, :format => 'json'
+    assert_response :success
+    assert_equal @project.changesets, assigns(:changesets)
+  end
+
+  test "should get for anonymous users in json format with from and to params for public projects" do
+    @project.public = true
+    @project.save
+    get :index, :project_id => @project.to_param, :format => 'json',
+                :from => @changeset.id - 1, :to => @changeset.id
+    assert_response :success
+    assert_equal [@changeset], assigns(:changesets)
+  end
 end
