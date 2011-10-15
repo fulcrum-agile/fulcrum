543a27e904f29d6a968fb63c9ff7831d5fd25a36
Bradley Priest <bradleypriest@gmail.com>
2011-10-16 12:14:08 +1300

    Use Compass spriting to remove hardcoded images from the html and css

--- config/compass.rb
+++ config/compass.rb
@@ -5,11 +5,13 @@ project_type = :rails
 # Set this to the root of your project when deployed:
 http_path = "/"
 
+http_images_path = "/images"
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = true
+# relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
--- public/javascripts/views/story_view.js
+++ public/javascripts/views/story_view.js
@@ -36,8 +36,8 @@ var StoryView = FormView.extend({
   },
 
   events: {
-    "click img.expand": "startEdit",
-    "click img.collapse": "saveEdit",
+    "click a.expand": "startEdit",
+    "click a.collapse": "saveEdit",
     "click #submit": "saveEdit",
     "click #cancel": "cancelEdit",
     "click .transition": "transition",
@@ -220,7 +220,7 @@ var StoryView = FormView.extend({
       div = this.make('div');
       if (!this.model.isNew()) {
         $(div).append(
-          this.make("img", {'class': "collapse", 'src': "/images/collapse.png"})
+          this.make("a", {'class': "collapse icon icons-collapse"})
         );
       }
       $(div).append(this.textField("title"));
@@ -300,11 +300,11 @@ var StoryView = FormView.extend({
 
   disableForm: function() {
     $(this.el).find('input,select,textarea').attr('disabled', 'disabled');
-    $(this.el).find('img.collapse,img.expand').attr('src', '/images/throbber.gif');
+    $(this.el).find('a.collapse,a.expand').removeClass(/icons-/).addClass('icons-throbber');
   },
 
   enableForm: function() {
-    $(this.el).find('img.collapse').attr('src', '/images/collapse.png');
+    $(this.el).find('a.collapse').removeClass(/icons-/).addClass("icons-collapse");
   },
 
   hoverBox: function(){
