62044e90efece20b9f0c84cb33ad4c7a43d60e91
Malcolm Locke <malc@wholemeal.co.nz>
2011-10-25 09:24:28 +1300

    Create a story changeset on note save

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,10 @@
 class Note < ActiveRecord::Base
   belongs_to :user
   belongs_to :story
+
+  after_save :create_changeset
+
+  def create_changeset
+    story.changesets.create! if story
+  end
 end
--- test/unit/note_test.rb
+++ test/unit/note_test.rb
@@ -6,6 +6,12 @@ class NoteTest < ActiveSupport::TestCase
     @project = Factory.create(:project, :users => [@user])
     @story = Factory.create(:story, :project => @project,
                             :requested_by => @user)
-    @note = Factory.create(:note, :story => @story, :author => @user)
+    @note = Factory.create(:note, :story => @story, :user => @user)
+  end
+
+  test "creating a note creates a changeset" do
+    assert_difference ['Changeset.count', '@story.changesets.count'] do
+      Factory.create(:note, :story => @story, :user => @user)
+    end
   end
 end
