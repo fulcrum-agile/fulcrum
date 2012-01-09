00fedfcb6306f6c824cae784aff8e70cd1ba6e77
Steven Talcott Smith <steve@aelogica.com>
2012-01-09 18:02:31 +0800

    [fix #80] Use local time for test
    Date formatter on story converts +0000 to local time for display

--- spec/javascripts/models/story.spec.js
+++ spec/javascripts/models/story.spec.js
@@ -252,7 +252,7 @@ describe('Story model', function() {
 
     it("should return a readable created_at", function() {
 
-      this.story.set({'created_at': "2011/09/19 02:25:56 +0000"});
+      this.story.set({'created_at': "2011/09/19 14:25:56"});
       expect(this.story.created_at()).toBe("19 Sep 2011, 2:25pm");
 
     });
