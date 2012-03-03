eac2a0271e7509a0315ef4098eb21c3b716cdd05
Malcolm Locke <malc@wholemeal.co.nz>
2012-03-04 11:10:30 +1300

    Migrate User tests to rspec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe User do
+
+  describe "validations" do
+    
+    it "requires a name" do
+      subject.name = ''
+      subject.should have(1).error_on(:name)
+    end
+
+    it "requires initials" do
+      subject.initials = ''
+      subject.should have(1).error_on(:initials)
+    end
+
+  end
+
+
+  describe "#to_s" do
+
+    subject { Factory.build(:user, :name => "Dummy User", :initials => "DU",
+                                    :email => "dummy@example.com") }
+
+    its(:to_s) { should == "Dummy User (DU) <dummy@example.com>" }
+
+  end
+
+  describe "#as_json" do
+
+    before do
+      subject.id = 42
+    end
+
+    specify {
+      subject.as_json['user'].keys.sort.should == %w[email id initials name]
+    }
+
+  end
+
+end
