be40d50a54cd69641a0b19291ebb77cdde38658d
Malcolm Locke <malc@wholemeal.co.nz>
2012-03-06 23:37:49 +1300

    Update deprecated factory syntax

--- test/factories.rb
+++ test/factories.rb
@@ -1,29 +1,33 @@
-Factory.define :user do |u|
-  u.sequence(:name) {|n| "User #{n}"}
-  u.sequence(:initials) {|n| "U#{n}"}
-  u.sequence(:email) {|n| "user#{n}@example.com"}
-  u.password 'password'
-  u.password_confirmation 'password'
-  u.after_build {|user| user.confirm!}
-end
+FactoryGirl.define do
 
-Factory.define :project do |p|
-  p.name 'Test Project'
-end
+  factory :user do |u|
+    u.sequence(:name) {|n| "User #{n}"}
+    u.sequence(:initials) {|n| "U#{n}"}
+    u.sequence(:email) {|n| "user#{n}@example.com"}
+    u.password 'password'
+    u.password_confirmation 'password'
+    u.after_build {|user| user.confirm!}
+  end
 
-Factory.define :story do |s|
-  s.title 'Test story'
-  s.association :requested_by, :factory => :user
-  s.association :project
-end
+  factory :project do |p|
+    p.name 'Test Project'
+  end
 
-Factory.define :changeset do |c|
-  c.association :story
-  c.association :project
-end
+  factory :story do |s|
+    s.title 'Test story'
+    s.association :requested_by, :factory => :user
+    s.association :project
+  end
 
-Factory.define :note do |n|
-  n.note        'Test note'
-  n.association :story
-  n.association :user
+  factory :changeset do |c|
+    c.association :story
+    c.association :project
+  end
+
+  factory :note do |n|
+    n.note        'Test note'
+    n.association :story
+    n.association :user
+  end
+
 end
