7b3af036205df9006c24fc030334b249d571ae4b
Malcolm Locke <malc@wholemeal.co.nz>
2012-03-16 23:47:32 +1300

    Add confirmations request specs
    Integration specs for account confirmation

--- spec/factories.rb
+++ spec/factories.rb
@@ -9,6 +9,12 @@ FactoryGirl.define do
     u.after_build {|user| user.confirm!}
   end
 
+  factory :unconfirmed_user, :class => User do |u|
+    u.sequence(:name) {|n| "Unconfirmed User #{n}"}
+    u.sequence(:initials) {|n| "U#{n}"}
+    u.sequence(:email) {|n| "unconfirmed_user#{n}@example.com"}
+  end
+
   factory :project do |p|
     p.name 'Test Project'
   end
