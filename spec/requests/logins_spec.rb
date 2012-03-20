7c7cfa2b9e2cc253abcf25177e62b5df3a2db5d8
Malcolm Locke <malc@wholemeal.co.nz>
2012-03-20 22:26:03 +1300

    Add :method => :delete to log out link
    Also adds integration spec for logout

--- spec/requests/logins_spec.rb
+++ spec/requests/logins_spec.rb
@@ -2,18 +2,20 @@ require 'spec_helper'
 
 describe "Logins" do
 
+  include IntegrationHelpers
+
   self.use_transactional_fixtures = false
 
   before(:each) do
     DatabaseCleaner.clean
   end
 
-  describe "successful login" do
+  let(:user)  {
+    FactoryGirl.create :user, :email => 'user@example.com',
+                              :password => 'password'
+  }
 
-    let(:user)  {
-      FactoryGirl.create :user, :email => 'user@example.com',
-                                :password => 'password'
-    }
+  describe "successful login" do
 
     before { user }
 
@@ -30,4 +32,18 @@ describe "Logins" do
     end
 
   end
+
+  describe "successful logout", :js => true do
+    before do
+      sign_in user
+    end
+
+    it "logs out the user" do
+      visit root_path
+      click_on 'Log out'
+
+      page.should have_selector('h1', :text => 'Sign in')
+    end
+  end
+
 end
