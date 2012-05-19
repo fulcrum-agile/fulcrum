2370427a3de16a1183f644f6624e6b3a5f9f06d3
Dave Perrett <dave@recurser.com>
2012-05-19 19:43:00 +1200

    Only apply body overflow hidden on project-show page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
       flash.collect { |index, value| content_tag(:div, value, {:class => "flash-" + index.to_s}) }.join.html_safe
     end unless flash.blank?
   end
+  
+  def page_id
+    "#{controller_name}_#{action_name}"
+  end
 end
