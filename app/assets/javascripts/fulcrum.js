0a68d8989d002bc5bb79dcf85df1d33fc5a1f287
Malcolm Locke <malc@wholemeal.co.nz>
2012-07-17 10:09:55 +1200

    Move sortable column collection (Fixes #113)

--- app/assets/javascripts/fulcrum.js
+++ app/assets/javascripts/fulcrum.js
@@ -8,10 +8,6 @@ $(function() {
     $('#chilly_bin').scrollTo(newStoryElement, 100);
   });
 
-  $('#backlog').sortable('option', 'connectWith', '#chilly_bin,#in_progress');
-  $('#chilly_bin').sortable('option', 'connectWith', '#backlog,#in_progress');
-  $('#in_progress').sortable('option', 'connectWith', '#backlog,#chilly_bin');
-
   // Add close button to flash messages
   $('#messages div').prepend('<a class="close" href="#">×</a>').find('a.close').click(function () {
     $(this).parent().fadeOut();
