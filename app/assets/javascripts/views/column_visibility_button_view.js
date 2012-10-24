25ea9bde0e82e0e1ef445d549c23dccefb08ffbb
Achilles Charmpilas <ac@humbuckercode.co.uk>
2012-10-24 14:54:50 +0200

    Fixed class set for VisibilityColumns, as add_story did not actually work as expected up till now (would not open chilly bin if it was closed)

--- app/assets/javascripts/views/column_visibility_button_view.js
+++ app/assets/javascripts/views/column_visibility_button_view.js
@@ -16,6 +16,7 @@ Fulcrum.ColumnVisibilityButtonView = Backbone.View.extend({
 
   initialize: function() {
     _.bindAll(this, 'setClassName');
+    this.$el.attr('class','hide_'+this.options.columnView.id);
     this.options.columnView.bind('visibilityChanged', this.setClassName);
   },
 
