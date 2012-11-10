af91fa52d2f9eec9df057f1cd6843786d3b3a435
Kale Worsley <kale@egressive.com>
2012-11-11 09:44:14 +1300

    Add a couple of controller specs for disabled user registration.

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -6,15 +6,52 @@ describe RegistrationsController do
     request.env['devise.mapping'] = Devise.mappings[:user]
   end
 
-  describe "#create" do
-    specify do
-      post :create, :user => {:name => 'Test User', :initials => 'TU', :email => 'test_user@example.com'}
-      response.should redirect_to(new_user_session_path)
+  describe "disable registration" do
+    before do
+      Configuration.for('fulcrum') do
+        disable_registration true
+      end
     end
-    specify do
-      post :create, :user => {:name => 'Test User', :initials => 'TU', :email => 'test_user@example.com'}
-      flash[:notice].should == 'You have signed up successfully. A confirmation was sent to your e-mail. Please follow the contained instructions to activate your account.'
+
+    describe "#new" do
+      specify do
+        get :new
+        response.status.should eq 404
+      end
+    end
+
+    describe "#create" do
+      specify do
+        post :create, :user => {:name => 'Test User', :initials => 'TU', :email => 'test_user@example.com'}
+        response.status.should eq 404
+      end
     end
   end
 
+  describe "enable registration" do
+    before do
+      Configuration.for('fulcrum') do
+        disable_registration false
+      end
+    end
+
+    describe "#new" do
+      specify do
+        get :new
+        response.status.should eq 200
+      end
+    end
+
+    describe "#create" do
+      specify do
+        post :create, :user => {:name => 'Test User', :initials => 'TU', :email => 'test_user@example.com'}
+        response.should redirect_to(new_user_session_path)
+      end
+      specify do
+        post :create, :user => {:name => 'Test User', :initials => 'TU', :email => 'test_user@example.com'}
+        flash[:notice].should == 'You have signed up successfully. A confirmation was sent to your e-mail. Please follow the contained instructions to activate your account.'
+      end
+    end
+
+  end
 end
