117b02ca0eff0d43045a80f11889485b06a6f4b8
ZakharDay <zakhar.day@gmail.com>
2013-02-03 03:02:11 +0400

    Focus on Title

--- app/assets/javascripts/views/story_view.js
+++ app/assets/javascripts/views/story_view.js
@@ -263,6 +263,7 @@ Fulcrum.StoryView = Fulcrum.FormView.extend({
       this.$el.append(
         this.makeFormControl(function(div) {
           $(div).append(this.textField("title", {
+            'class' : 'title',
             'placeholder': I18n.t('story title')
           }));
         })
@@ -489,7 +490,7 @@ Fulcrum.StoryView = Fulcrum.FormView.extend({
 
   setFocus: function() {
     if (this.model.get('editing') === true ) {
-      this.$('input').first().focus();
+      this.$('input.title').first().focus();
     }
   },
 
