c1ab2ba4c89cab98cc0e0b834e76e5bd2ba650a0
Achilles Charmpilas <ac@humbuckercode.co.uk>
2013-03-09 10:32:56 -0800

    send_keys now works with webkit

--- spec/support/integration_helpers.rb
+++ spec/support/integration_helpers.rb
@@ -8,7 +8,26 @@ module IntegrationHelpers
   end
   
   def send_keys keys, options = {}
-    element = options[:element] ? options.delete(:element) : find('html')
-    element.native.send_keys keys
+    keycode = case keys
+      when '?'
+        63
+      when 'B'
+        66
+      when 'C'
+        67
+      when 'D'
+        68
+      when 'P'
+        80
+      when 'a'
+        97
+      when :pause
+        19
+      else
+        keys
+    end
+    element = options[:element] ? options.delete(:element) : 'body'
+    keypress_script = "var e = jQuery.Event('keypress', { keyCode: #{keycode} }); jQuery('#{element}').trigger(e);"
+    page.driver.browser.execute_script(keypress_script)
   end
 end
