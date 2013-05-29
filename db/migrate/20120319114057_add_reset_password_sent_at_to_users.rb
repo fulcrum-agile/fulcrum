d7eaa4725ffa1478ec00d064e97cf2803211fb38
Gustavo Chain <gchain@gmail.com>
2013-05-29 12:50:34 -0400

    Prevent migration crashes on new install

--- db/migrate/20120319114057_add_reset_password_sent_at_to_users.rb
+++ db/migrate/20120319114057_add_reset_password_sent_at_to_users.rb
@@ -1,6 +1,8 @@
 class AddResetPasswordSentAtToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :reset_password_sent_at, :datetime
+    if !column_exists?(:users, :reset_password_sent_at)
+      add_column :users, :reset_password_sent_at, :datetime
+    end
 
   end
 end
