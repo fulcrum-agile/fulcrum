c45257be54c76f79b9ced82fb47cc0adaed072cb
Dom Stockdale <dom.stockdale@gmail.com>
2013-11-10 19:10:21 +1100

    Adds missing config.secret_key_base for rails 4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Fulcrum::Application.config.secret_token = '2f0d0a139bff42c286f5f4af2910d03aebd461f7d72bab018d6d9095f7f578c04b2f8041923eeb62e2246efbaf7468c3e93f65f3fa007f06b95ff867a52330bd'
+Fulcrum::Application.config.secret_key_base = '83ad195d0e1cb2e695f6f6258f71fb3564826f1363caa5f6f91824d3049791f1953734cbe5c73e3728c7f7b625e157bcdc2e64a4d94b60c5e2632805af48673c'
