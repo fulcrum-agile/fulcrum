e2b6f3b899ceae2d514c5a0a28bd3ba5f84a6dba
Dom Stockdale <dom.stockdale@gmail.com>
2013-11-24 13:28:10 +1100

    Changed call scoped to all to remove deprecation warnings

--- app/controllers/changesets_controller.rb
+++ app/controllers/changesets_controller.rb
@@ -2,7 +2,7 @@ class ChangesetsController < ApplicationController
   def index
     @project = current_user.projects.find(params[:project_id])
     # FIXME extract method to model
-    scope = @project.changesets.scoped
+    scope = @project.changesets.all
     scope = scope.where('id <= ?', params[:to]) if params[:to]
     scope = scope.where('id > ?', params[:from]) if params[:from]
     @changesets = scope
