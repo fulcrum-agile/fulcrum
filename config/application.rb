ac99c513fa05e1c66825f7da9a2f02366b8d48d1
Malcolm Locke <malc@wholemeal.co.nz>
2014-01-17 23:11:28 +1300

    Silence deprecation for enforce_available_locales

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,7 @@ module Fulcrum
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.enforce_available_locales = true
     config.i18n.available_locales = ['de', 'el', 'en', 'es', 'nl', 'ja']
 
     # Configure the default encoding used in templates for Ruby 1.9.
