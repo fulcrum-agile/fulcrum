25f998641c1dc40b3a424b9bb0aeeaa895b42602
AkitaOnRails <fabioakita@gmail.com>
2015-06-23 12:09:34 -0300

    Adding SendGrid configuration Adding Fulcrum customisation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,16 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => config.fulcrum.app_host }
 
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
