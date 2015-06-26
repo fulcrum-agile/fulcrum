cb14463d58fc0d5f7414b36a21c9bc3d7046b632
Alessandro <alessandrodbatista@gmail.com>
2015-06-26 11:40:18 -0300

    fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -88,12 +88,12 @@ ActiveRecord::Schema.define(version: 20150626134654) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                              default: "",   null: false
-    t.string   "encrypted_password",     limit: 128, default: "",   null: false
+    t.string   "email",                  default: "",   null: false
+    t.string   "encrypted_password",     default: "",   null: false
     t.string   "reset_password_token"
-    t.string   "remember_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -101,15 +101,14 @@ ActiveRecord::Schema.define(version: 20150626134654) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "password_salt"
+    t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
     t.string   "initials"
-    t.boolean  "email_delivery",                     default: true
-    t.boolean  "email_acceptance",                   default: true
-    t.boolean  "email_rejection",                    default: true
-    t.datetime "reset_password_sent_at"
+    t.boolean  "email_delivery",         default: true
+    t.boolean  "email_acceptance",       default: true
+    t.boolean  "email_rejection",        default: true
     t.string   "locale"
   end
 
