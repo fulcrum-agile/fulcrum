2a5b5277e09a29f298fce520bd2cf63d2dc1a469
AkitaOnRails <fabioakita@gmail.com>
2015-06-30 16:47:35 -0300

    Adding a memcache connection pool - to avoid depleting the available connections in case the number of Sidekiq threads grow too much

--- app/workers/import_worker.rb
+++ app/workers/import_worker.rb
@@ -1,17 +1,27 @@
 class ImportWorker
   include Sidekiq::Worker
 
+  MEMCACHED_POOL = ConnectionPool.new(:size => 10, :timeout => 3) do
+                     Rails.cache.try(:dalli) || Dalli::Client.new
+                   end
+
   def perform(job_id, project_id)
-    Rails.cache.write(job_id, nil)
+    set_cache(job_id, nil)
     @project = Project.friendly.find(project_id)
+
     # Do not send any email notifications during the import process
     @project.suppress_notifications = true
+
     Project.transaction do
       @stories = @project.stories.from_csv(open(@project.import.fullpath).read)
-      Rails.cache.write(job_id, { stories: @stories, errors: nil },
-                        time_to_idle: 1.minute, timeToLive: 1.hour)
+      set_cache(job_id, { stories: @stories, errors: nil })
     end
   rescue => e
-    Rails.cache.write(job_id, { stories: [], errors: e })
+    set_cache(job_id, { stories: [], errors: e })
   end
+
+  private
+    def set_cache(key, value)
+      MEMCACHED_POOL.with { |dalli| dalli.set(key, value) }
+    end
 end
