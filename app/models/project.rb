6a8c175c0f23dd605a5cb22f4ee503551e0517a1
Pete Nicholls <aupajo@gmail.com>
2015-08-20 20:07:35 +1200

    Freeze the things
    @parndt guarantees this will make the app ROFLscale.
    
    - Edited on my iPhone

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,20 +3,23 @@ class Project < ActiveRecord::Base
   JSON_ATTRIBUTES = [
     "id", "iteration_length", "iteration_start_day", "start_date",
     "default_velocity"
-  ]
-  JSON_METHODS = ["last_changeset_id", "point_values"]
+  ].freeze
+
+  JSON_METHODS = ["last_changeset_id", "point_values"].freeze
 
   # These are the valid point scalse for a project.  These represent
   # the set of valid points estimate values for a story in this project.
   POINT_SCALES = {
-    'fibonacci'     => [0,1,2,3,5,8],
-    'powers_of_two' => [0,1,2,4,8],
-    'linear'        => [0,1,2,3,4,5],
-  }
+    'fibonacci'     => [0,1,2,3,5,8].freeze,
+    'powers_of_two' => [0,1,2,4,8].freeze,
+    'linear'        => [0,1,2,3,4,5].freeze,
+  }.freeze
+
   validates_inclusion_of :point_scale, :in => POINT_SCALES.keys,
     :message => "%{value} is not a valid estimation scheme"
 
-  ITERATION_LENGTH_RANGE = (1..4)
+  ITERATION_LENGTH_RANGE = (1..4).freeze
+
   validates_numericality_of :iteration_length,
     :greater_than_or_equal_to => ITERATION_LENGTH_RANGE.min,
     :less_than_or_equal_to => ITERATION_LENGTH_RANGE.max, :only_integer => true,
