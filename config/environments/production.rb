63866405fb3baf4326368ff2414ab478356f4dae
Fabio Akita <fabioakita@gmail.com>
2015-09-17 14:14:28 -0300

    Adding font_assets to send proper CORS headers Slight change in a CSS to trigger new precompilation fingerprints

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = "http://central-cdn.cm42.io"
+  config.font_assets.origin = 'http://central.cm42.io'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
