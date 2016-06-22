90a28197b39977043d51e774b345b4d359bbf620
Romulo Storel <romulostorel@gmail.com>
2016-06-22 15:06:14 -0300

    Refactor epic bar
    * Show done and remaining points

--- app/assets/javascripts/views/epic_bar_view.js
+++ app/assets/javascripts/views/epic_bar_view.js
@@ -9,17 +9,45 @@ Fulcrum.EpicBarView = Backbone.View.extend({
   className: 'iteration',
 
   render: function() {
-    this.$el.html(this.template({stories: this.model.search.length, points: this.points()}));
+    this.$el.html(this.template({points: this.points(), done: this.donePoints(), remaining: this.remainingPoints()}));
     return this;
   },
 
   points: function() {
     var estimates = this.model.search.pluck('estimate')
+
+    return this.sumPoints(estimates);
+  },
+
+  donePoints: function() {
+    var estimates = _.map(this.done(), function(e) { return e.get('estimate') })
+
+    return this.sumPoints(estimates);
+  },
+
+  remainingPoints: function(){
+    var estimates = _.map(this.remaining(), function(e) { return e.get('estimate') })
+
+    return this.sumPoints(estimates);;
+  },
+
+  done: function() {
+    return _.select(this.model.search.models, function(story) {
+      return (story.get('state') === 'accepted');
+    });
+  },
+
+  remaining: function(){
+    return _.select(this.model.search.models, function(story) {
+      return (story.get('state') != 'accepted');
+    });
+  },
+
+  sumPoints: function(estimates) {
     var sum = _.reduce(estimates, function(total, estimate) {
       return total + estimate;
     })
+
     return sum;
   }
-
 });
-
