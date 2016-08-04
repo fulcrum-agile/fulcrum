a209c229db2c026a07f406770ea90926cb833931
Danilo Resende <danilo.isr@gmail.com>
2016-08-04 16:01:20 -0300

    Move codeclimate configs to a support file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,3 @@
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
