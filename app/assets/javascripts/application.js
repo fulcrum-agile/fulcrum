362c67d74d0fd37a3ddb2bc4af3389ddc554d179
Fabio Akita <fabioakita@gmail.com>
2016-08-18 16:59:54 -0300

    Tried to add the JST template as a partial without the .erb extension but the #render method still tries to evaluate it. So reading directly from the file and embedding.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,33 +33,5 @@ $(function() {
 });
 
 function executeAttachinary() {
-  var js_template = [
-    "<ul class='attachinary_images_list'>",
-    "<% if ( files.length == 0 ) { %>",
-    "  <li>zero documents uploaded yet</li>",
-    "<% } %>",
-    "<% for(var i = 0; i < files.length; i++) { %>",
-    "  <li>",
-    "    <% if ( files[i] && files[i].resource_type == \"raw\" ) { %>",
-    "      <div class=\"raw-file\">",
-    "        Document: ",
-    "        <a href=\"<%= $.cloudinary.url(files[i].public_id, { resource_type: 'raw' }) %>\" target=\"_blank\">",
-    "           <%= files[i].public_id %>",
-    "        </a>",
-    "        <a href=\"#\" data-remove=\"<%= files[i].public_id %>\">Remove</a>",
-    "      </div>",
-    "    <% } else { %>",
-    "      <a href=\"<%= $.cloudinary.url(files[i].public_id) %>\" target=\"_blank\">",
-    "        <img src=\"<%= $.cloudinary.url(files[i].public_id, { \"version\": files[i].version, \"format\": 'jpg', \"crop\": 'fill', \"width\": 75, \"height\": 75 }) %>\"",
-    "        alt=\"\" width=\"75\" height=\"75\" />",
-    "      </a>",
-    "      <a href=\"#\" data-remove=\"<%= files[i].public_id %>\">Remove</a>",
-    "    <% } %>",
-    "  </li>",
-    "<% } %>",
-    "</ul>"
-    ].join('\n');
-  return $('.attachinary-input').attachinary({
-    template: js_template
-  });
+  return $('.attachinary-input').attachinary({ template: $('#attachinary_template').html() });
 }
