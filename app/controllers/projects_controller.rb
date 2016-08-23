94a58f130248d454a6d702ba866b4282bbabe64b
Fabio Akita <fabioakita@gmail.com>
2016-08-23 16:44:43 -0300

    Adding a rudimentar version of the iteration builder copied from the JS equivalent to be able to make simple forecasts.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -150,7 +150,13 @@ class ProjectsController < ApplicationController
   def reports
     since = params[:since].nil? ? nil : params[:since].to_i.months.ago
     @service = IterationService.new(@project, since)
-    current_month = (@service.current_iteration / 4).floor
+    current_month = (@service.current_iteration_number / 4).floor
+    @current_iteration_start_date = @service.date_for_iteration_number(@service.current_iteration_number).to_date.to_s(:short)
+    @current_iteration_end_date   = (@service.date_for_iteration_number(@service.current_iteration_number + 1) - 1.day).to_date.to_s(:short)
+    @current_iteration            = @service.iteration_details(@service.backlog_iterations.first)
+    @accepted_points              = @service.current_iteration_details['accepted']
+    @accepted_rate                = @service.current_iteration_details['accepted'] / @current_iteration[:points]
+    @last_iteration               = @service.backlog_iterations.last
 
     @since_options = if current_month > 6
         [1,3,6]
--- app/services/iteration_service.rb
+++ app/services/iteration_service.rb
@@ -1,7 +1,50 @@
 class IterationService
-  DAYS_IN_WEEK = (1.week / 1.day)
 
-  attr_reader :project, :stories
+  # mimics the iteration.js counterpart
+  class Iteration < Array
+    attr_reader :start_date, :number, :maximum_points
+
+    def initialize(service, iteration_number, maximum_points = nil)
+      @service        = service
+      @number         = iteration_number
+      @maximum_points = maximum_points
+      @is_full        = false
+      super([])
+    end
+
+    def points
+      self.reduce(0) { |total, story| total + ( story.estimate || 0 ) }
+    end
+
+    def available_points
+      maximum_points - points
+    end
+
+    def can_take_story?(story)
+      return true if %w(started finished delivered accepted rejected).include? story.state
+      return false if @is_full
+      return true if points == 0
+      return true if story.story_type != 'feature'
+
+      @is_full = (story.estimate || 0) > available_points
+      !@is_full
+    end
+
+    def overflows_by
+      difference = points - maximum_points
+      difference < 0 ? 0 : difference
+    end
+
+    def start_date
+      @service.date_for_iteration_number(@number)
+    end
+  end
+
+
+  DAYS_IN_WEEK = (1.week / 1.day)
+  VELOCITY_ITERATIONS = 3
+
+  attr_reader :project
 
   delegate :start_date, :start_date=,
     :iteration_length, :iteration_length=,
@@ -10,19 +53,15 @@ class IterationService
 
   def initialize(project, since = nil)
     @project = project
-    @stories = accepted_stories(since).to_a
+    @stories = project.stories.includes(:owned_by).to_a
+
+    @accepted_stories = @stories.
+      select { |story| story.column == '#done' }.
+      select { |story| story.accepted_at < iteration_start_date(Time.current) }
     calculate_iterations!
     fix_owner!
-  end
 
-  def accepted_stories(since = nil)
-    relation = project.stories.includes(:owned_by).
-      where.not(accepted_at: nil).
-      order(:accepted_at).
-      where("accepted_at < ?", iteration_start_date(Time.current))
-
-    relation = relation.where("accepted_at > ?", since) if since
-    relation
+    @backlog = ( @stories - @accepted_stories ).sort_by(&:position)
   end
 
   def iteration_start_date(date = nil)
@@ -31,14 +70,15 @@ class IterationService
     if start_date.wday != iteration_start_day
       day_difference = start_date.wday - iteration_start_day
       day_difference += DAYS_IN_WEEK if day_difference < 0
+
       iteration_start_date -= day_difference.days
     end
     iteration_start_date
   end
 
   def iteration_number_for_date(compare_date)
-    compare_date = compare_date.to_time if compare_date.is_a?(Date)
-    days_apart = ( compare_date - iteration_start_date ) / 1.day
+    compare_date      = compare_date.to_time if compare_date.is_a?(Date)
+    days_apart        = ( compare_date - iteration_start_date ) / 1.day
     days_in_iteration = iteration_length * DAYS_IN_WEEK
     ( days_apart / days_in_iteration ).floor + 1
   end
@@ -48,15 +88,15 @@ class IterationService
     iteration_start_date + difference.days
   end
 
-  def current_iteration
+  def current_iteration_number
     iteration_number_for_date(Time.current)
   end
 
   def calculate_iterations!
-    @stories.each do |record|
-      iteration_number = iteration_number_for_date(record.accepted_at)
-      iteration_start_date = date_for_iteration_number(iteration_number)
-      record.iteration_number = iteration_number
+    @accepted_stories.each do |record|
+      iteration_number            = iteration_number_for_date(record.accepted_at)
+      iteration_start_date        = date_for_iteration_number(iteration_number)
+      record.iteration_number     = iteration_number
       record.iteration_start_date = iteration_start_date
     end
   end
@@ -64,13 +104,13 @@ class IterationService
   # FIXME must figure out why the Story allows a nil owner in delivered states
   def fix_owner!
     @dummy_user ||= User.find_or_create_by!(username: "dummy", email: "dummy@foo.com", name: "Dummy", initials: "XX")
-    @stories.
+    @accepted_stories.
       select { |record| record.owned_by.nil? }.
       each   { |record| record.owned_by = @dummy_user }
   end
 
   def group_by_iteration
-    @group_by_iteration ||= @stories.
+    @group_by_iteration ||= @accepted_stories.
       group_by { |story| story.iteration_number }.
       reduce({}) do |group, iteration|
         group.merge(iteration.first => stories_estimates(iteration.last))
@@ -88,13 +128,8 @@ class IterationService
   end
 
   def group_by_velocity
-    @group_by_velocity ||= group_by_iteration.keys.reduce({}) do |group, key|
-      begin
-        group.merge(key => group_by_iteration[key].reduce(&:+))
-      rescue => e
-        # FIXME should investigate why this fails (nil estimate on estimable stories)
-        Rails.logger.error("[IterationService#group_by_velocity] #{key} #{group_by_iteration[key]}")
-      end
+    @group_by_velocity ||= group_by_iteration.reduce({}) do |group, iteration|
+      group.merge(iteration.first => iteration.last.reduce(&:+))
     end
   end
 
@@ -109,27 +144,20 @@ class IterationService
   end
 
   def group_by_bugs
-    @group_by_bugs ||= begin
-      by_bugs = @stories.
-        group_by { |story| story.iteration_number }.
-        reduce({}) do |group, iteration|
-          group.merge(iteration.first => bugs_impact(iteration.last))
-        end
-      by_bugs.reduce({}) do |group, iteration|
-        begin
-          group.merge(iteration.first => iteration.last.reduce(&:+))
-        rescue => e
-          # FIXME should investigate why this fails (nil estimate on estimable stories)
-          Rails.logger.error("[IterationService#group_by_bugs] #{key} #{iteration.last}")
-        end
+    @group_by_bugs ||=  @accepted_stories.
+      group_by { |story| story.iteration_number }.
+      reduce({}) do |group, iteration|
+        group.merge(iteration.first => bugs_impact(iteration.last))
+      end.
+      reduce({}) do |group, iteration|
+        group.merge(iteration.first => iteration.last.reduce(&:+))
       end
-    end
   end
 
   def velocity
     @velocity ||= begin
       iterations = group_by_iteration.size
-      iterations = 3 if iterations > 3
+      iterations = VELOCITY_ITERATIONS if iterations > VELOCITY_ITERATIONS
 
       sum = group_by_velocity.values.slice((-1 * iterations)..-1).
         reduce(&:+)
@@ -143,8 +171,8 @@ class IterationService
   end
 
   def group_by_developer
-    @group_by_developer ||= @stories.
-      group_by { |o| o.owned_by.name }.
+    @group_by_developer ||= @accepted_stories.
+      group_by { |story| story.owned_by.name }.
       reduce([]) do |group, owner|
         data = owner.last.group_by { |story| story.iteration_number }.
           reduce({}) do |group, iteration|
@@ -154,4 +182,47 @@ class IterationService
         group << { name: owner.first, data: data }
       end
   end
+
+  def backlog_iterations
+    # mimics the project.js rebuildIteration() function
+    @backlog_iterations ||= begin
+      current_iteration = Iteration.new(self, current_iteration_number, velocity)
+      backlog_iteration = Iteration.new(self, current_iteration_number + 1, velocity)
+      [current_iteration, backlog_iteration].tap do |iterations|
+        @backlog.each do |story|
+          if current_iteration.can_take_story?(story)
+            current_iteration << story
+          else
+            if !backlog_iteration.can_take_story?(story)
+              # Iterations sometimes 'overflow', i.e. an iteration may contain a
+              # 5 point story but the project velocity is 1.  In this case, the
+              # next iteration that can have a story added is the current + 4.
+              next_number       = backlog_iteration.number + 1 + (backlog_iteration.overflows_by / velocity).ceil
+              backlog_iteration = Iteration.new(self, next_number, velocity)
+              iterations << backlog_iteration
+            end
+            backlog_iteration << story
+          end
+        end
+      end
+    end
+  end
+
+  def current_iteration_details
+    current_iteration = backlog_iterations.first
+    %w(started finished delivered accepted rejected).reduce({}) do |data, state|
+      data.merge(state => current_iteration.
+                 select { |story| story.state == state }.
+                 reduce(0) { |points, story| points + (story.estimate || 0) } )
+    end
+  end
+
+  def iteration_details(iteration)
+    {
+      points: iteration.reduce(0) { |total, story| total + (story.estimate || 0) },
+      count: iteration.size,
+      non_estimable: iteration.select { |story| !Story::ESTIMABLE_TYPES.include?(story.story_type) }.size
+    }
+  end
 end
+
