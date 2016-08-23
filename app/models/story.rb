e49fbdcdf6273845691ec7fa1d1cc3e9845c407c
Fabio Akita <fabioakita@gmail.com>
2016-08-23 11:21:37 -0300

    Adjusting the iteration calculation to use AR objects (heavier) instead of prematurely optimising into arrays and hashes. Adding proper grouping calculation for points, velocity per iteration and velocity per member per iteration

--- app/models/story.rb
+++ app/models/story.rb
@@ -96,6 +96,7 @@ class Story < ActiveRecord::Base
   # This attribute is used to store the user who is acting on a story, for
   # example delivering or modifying it.  Usually set by the controller.
   attr_accessor :acting_user, :base_uri
+  attr_accessor :iteration_number, :iteration_start_date # helper fields for IterationService
 
   STORY_TYPES = [
     'feature', 'chore', 'bug', 'release'
--- app/services/iteration_service.rb
+++ app/services/iteration_service.rb
@@ -1,6 +1,5 @@
 class IterationService
   DAYS_IN_WEEK = (1.week / 1.day)
-  FIELDS = [:id, :estimate, :accepted_at, :story_type]
 
   attr_reader :project, :stories
 
@@ -11,10 +10,12 @@ class IterationService
 
   def initialize(project)
     @project = project
-    @stories = project.stories.
+    @stories = project.stories.includes(:owned_by).
       where.not(accepted_at: nil).
       order(:accepted_at).
-      pluck(*FIELDS)
+      to_a
+    calculate_iterations!
+    fix_owner!
   end
 
   def iteration_start_date
@@ -39,21 +40,75 @@ class IterationService
     iteration_start_date + difference.days
   end
 
-  def stories_hash
-    @stories.
-      map { |record| Hash[FIELDS.zip(record)] }.
-      map do |hash|
-        iteration_number = iteration_number_for_date(hash[:accepted_at])
-        iteration_start_date = date_for_iteration_number(iteration_number)
-        hash.merge(
-          iteration_number: iteration_number,
-          iteration_start_date: iteration_start_date)
+  def calculate_iterations!
+    @stories.each do |record|
+      iteration_number = iteration_number_for_date(record.accepted_at)
+      iteration_start_date = date_for_iteration_number(iteration_number)
+      record.iteration_number = iteration_number
+      record.iteration_start_date = iteration_start_date
+    end
+  end
+
+  def fix_owner!
+    @dummy_user ||= User.find_or_create_by!(username: "dummy", email: "dummy@foo.com", name: "Dummy")
+    @stories.each do |record|
+      record.owned_by = @dummy_user if record.owned_by.nil?
+    end
+  end
+
+  def group_by_iteration
+    @group_by_iteration ||= @stories.
+      group_by { |story| story.iteration_number }.
+      reduce({}) do |group, iteration|
+        points = iteration.last.
+          map { |story|
+            if %w(chore bug).include? story.story_type
+              0
+            else
+              story.estimate
+            end
+          }
+        group.merge(iteration.first => points)
       end
   end
 
   def group_by_velocity
-    stories_hash.
-      group_by { |o| o[:iteration_start_date] }.
-      inject({}) { |group, iteration| group.merge(iteration.first.to_date => iteration.last.size) }
+    @group_by_velocity ||= group_by_iteration.keys.reduce({}) do |group, key|
+      group.merge(key => group_by_iteration[key].reduce(&:+))
+    end
+  end
+
+  def velocity
+    @velocity ||= begin
+      iterations = group_by_iteration.size
+      iteration = 3 if iterations > 3
+      sum = group_by_velocity.values.slice((-1 * iteration)..-1).
+        reduce(0) { |total, points| total + points }
+      stories = group_by_iteration.values.slice((-1 * iteration)..-1).
+        reduce(0) { |total, stories| total + stories.size }
+      velocity = (sum / stories).floor
+      velocity < 1 ? 1 : velocity
+    end
+  end
+
+  def group_by_developer
+    @group_by_developer ||= @stories.
+      group_by { |o| o.owned_by.name }.
+      reduce([]) do |group, owner|
+        data = owner.last.group_by { |story| story.iteration_number }.
+          reduce({}) do |group, iteration|
+            points = iteration.last.
+              map { |story|
+                if %w(chore bug).include? story.story_type
+                  0
+                else
+                  story.estimate
+                end
+              }.
+              reduce(&:+)
+            group.merge(iteration.first => points)
+          end
+        group << { name: owner.first, data: data }
+      end
   end
 end
