af79aaae0710e25dac5e276cc26583c13dfa2f2b
Fabio Akita <fabioakita@gmail.com>
2016-09-20 11:26:11 -0300

    Removing Integration logic into central-support gem.

--- app/models/integration.rb
+++ app/models/integration.rb
@@ -1,8 +1,3 @@
 class Integration < ActiveRecord::Base
-  VALID_INTEGRATIONS = ['mattermost']
-
-  belongs_to :project
-  validates :project, presence: true
-  validates :kind, inclusion: { in: VALID_INTEGRATIONS }, presence: true
-  validates :data, presence: true
+  include Central::Support::IntegrationConcern
 end
--- spec/models/integration_spec.rb
+++ spec/models/integration_spec.rb
@@ -1,53 +1,5 @@
 require 'rails_helper'
 
-describe Integration do
+describe Integration, type: :model do
 
-  subject { build :integration }
-
-  describe "#project" do
-    it "cannot be nil" do
-      subject.project_id = nil
-      subject.valid?
-      expect(subject.errors[:project].size).to eq(1)
-    end
-
-    it "must have a valid project_id" do
-      subject.project_id = "invalid"
-      subject.valid?
-      expect(subject.errors[:project].size).to eq(1)
-    end
-
-    it "must have a project" do
-      subject.project =  nil
-      subject.valid?
-      expect(subject.errors[:project].size).to eq(1)
-    end
-  end
-
-  describe "#kind" do
-    it "should have a valid kind" do
-      subject.kind = 'foo'
-      subject.valid?
-      expect(subject.errors[:kind].size).to eq(1)
-    end
-
-    it "should have a kind" do
-      subject.kind = nil
-      subject.valid?
-      expect(subject.errors[:kind].size).to eq(2)
-    end
-  end
-
-  describe "#data" do
-    it "should have a valid serializable data field" do
-      payload = { channel: 'foo', bot_username: 'bar', private_uri: 'baz' }
-      subject.data = payload
-      subject.save
-      subject.reload
-
-      expect(subject.data['channel']).to eq('foo')
-      expect(subject.data['bot_username']).to eq('bar')
-      expect(subject.data['private_uri']).to eq('baz')
-    end
-  end
 end
