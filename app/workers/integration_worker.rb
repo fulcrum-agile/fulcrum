535d3da2fa8d101cc3d46279cc2a7e8455b36142
Fabio Akita <fabioakita@gmail.com>
2016-09-28 15:10:50 -0300

    moving mattermost helpers to the central-support gem

--- app/workers/integration_worker.rb
+++ app/workers/integration_worker.rb
@@ -1,26 +1,11 @@
 class IntegrationWorker
   include Sidekiq::Worker
+  include Central::Support::MattermostHelper
 
   def perform(project_id, message)
     project = Project.find(project_id)
-    project.integrations.each do |integration|
-      if integration.kind == 'mattermost'
-        Mattermost.send(real_private_uri(integration.data['private_uri'] ),
-                        integration.data['channel'],
-                        integration.data['bot_username'],
-                        message)
-      end
-    end
-  end
-
-  private
-
-  def real_private_uri(private_uri)
-    if private_uri.starts_with? "INTEGRATION_URI"
-      ENV[private_uri]
-    else
-      private_uri
-    end
+    project.integrations.
+      select { |integration| integration.kind == 'mattermost' }.
+      each   { |integration| send_mattermost(integration, message) }
   end
 end
-
