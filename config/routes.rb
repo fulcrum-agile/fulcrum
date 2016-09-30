f825800a3296b84cf04635a7d135cadb0b9fd65f
Danilo Resende <danilo.isr@gmail.com>
2016-09-30 18:53:40 -0300

    ActiveAdmin: Add memberships routes to projects

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace :manage do
     resources :projects do
       resources :ownerships
+      resources :memberships
     end
     resources :teams do
       resources :ownerships
