7f6559b39d0711381a7f2b08f309be2091584933
Danilo Resende <danilo.isr@gmail.com>
2016-10-03 11:03:37 -0300

    Bump assets files to fix CORS headers

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = '1.1'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
