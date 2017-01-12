6d7179a08dcde01c2935a9be8f59de393f1be25c
Edgar Yonemura <edgar.yonemura@gmail.com>
2017-01-12 15:13:13 -0200

    Fix projects list from top bar menu

--- app/helpers/topbar_helper.rb
+++ app/helpers/topbar_helper.rb
@@ -1,11 +1,11 @@
-module SidebarHelper
-  def sidebar_link(*args)
+module TopbarHelper
+  def topbar_link(*args)
     link_to_unless_current(*args) do
       link_to(args.first, '#', class: 'nav-current')
     end
   end
 
-  def sidebar_projects
+  def topbar_projects
     scope = ProjectPolicy::Scope.new(pundit_user, Project).resolve
     scope.not_archived.order(:name).each do |project|
       yield project
