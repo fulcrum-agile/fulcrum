a1946c4adb92d70a953f80da409614d0f90d13d5
Marcos Siqueira Jr <junior.crvg@gmail.com>
2017-01-23 13:19:34 -0300

    add edit project and creates a project settings page

--- app/helpers/navbar_helper.rb
+++ app/helpers/navbar_helper.rb
@@ -2,4 +2,12 @@ module NavbarHelper
   def active_class(link_path)
     current_page?(link_path) ? "active" : ""
   end
+
+  def settings_active_class(project)
+    unless active_class(edit_project_path(project)).empty? &&
+           active_class(import_project_path(project)).empty? &&
+           active_class(project_integrations_path(project)).empty?
+      return "active"
+    end
+  end
 end
